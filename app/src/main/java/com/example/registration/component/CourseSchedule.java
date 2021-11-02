047df0bca6800541895dde3a373eb754593bb9e0
Chanhuyng Shim <cshim31@gatech.edu>
2021-11-02 01:30:56 -0400

    Change design (#1)
    * fix: resolve course search engine not updating from default selection
    
    * feat: add statistic displaying all course from user record
    
    style: set schedule chips padding
    
    * fix: syntax
    
    * style: add semester filter design in statstic module
    
    * feat: Add semester filter functionality
    
    * chore: add missed class files
    
    * refactor: organize file and structure
    style: add semester indicator in statistic fragment
    
    * style: change font size and listview
    
    * build: add dropped build files and rebuild
    
    * style: change semester component width size
    
    * feat: complete delete course from statistic
    
    * fix: resolve error with time duplicate check
    
    * style: change style

---
+++ app/src/main/java/com/example/registration/component/CourseSchedule.java
@@ -0,0 +1,107 @@
+/*
+Copyright 2019 tlaabs
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+ */
+
+/*
+ *  1 2 3 4 5 .. 14
+ * :15
+ * :30
+ * :45
+ */
+package com.example.registration.component;
+
+import com.example.registration.util.util.Util;
+import com.github.tlaabs.timetableview.Schedule;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CourseSchedule extends Schedule {
+    private Map<Integer, Integer> parseTimedTime;
+
+    public CourseSchedule() {
+        super();
+    }
+
+    public CourseSchedule(String courseTitle, String courseInstructor, String courseLocation, String courseTime, char courseDay) {
+        this.parseTimedTime = new HashMap<Integer, Integer>();
+        setDay(courseDay);
+        setClassTitle(courseTitle);
+        setStartTime(new CourseTime(Util.split(courseTime,"-",2)[0]));
+        setEndTime(new CourseTime(Util.split(courseTime,"-",2)[1]));
+        setClassPlace(courseLocation);
+        setProfessorName(courseInstructor);
+    }
+
+
+    public void setDay(char day) {
+        switch (day) {
+            case 'M':
+                setDay(0);
+                break;
+
+            case 'T':
+                setDay(1);
+                break;
+
+            case 'W':
+                setDay(2);
+                break;
+
+            case 'R':
+                setDay(3);
+                break;
+
+            case 'F':
+                setDay(4);
+                break;
+        }
+    }
+
+    public String parseDay(int day) {
+        char parsed = ' ';
+        switch(day) {
+            case 0 :
+                parsed = 'M';
+                break;
+            case 1 :
+                parsed = 'T';
+                break;
+
+            case 2 :
+                parsed = 'W';
+                break;
+
+            case 3 :
+                parsed = 'H';
+                break;
+
+            case 4 :
+                parsed = 'F';
+                break;
+        }
+
+        return Character.toString(parsed);
+    }
+
+    /**
+     *  Additional variable setter & getter functions
+     */
+
+    public String getCourseTime() {
+        return getStartTime() + " : " + getEndTime();
+    }
+}
---
+++ app/src/main/java/com/example/registration/util/json/JsonReader.java
@@ -0,0 +1,71 @@
+package com.example.registration.util.json;
+
+import com.example.registration.component.Course;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JsonReader {
+    public List<Course> fetchCourse(String json) {
+        List<Course> list = new ArrayList<Course>();
+
+        if (json.isEmpty()) return list;
+
+        try {
+            String result = (String) json;
+            JSONObject jsonObject = new JSONObject(result);
+
+            JSONArray jsonResponse;
+
+            try {
+                jsonResponse = jsonObject.getJSONArray("courses");
+            } catch (Exception e) {
+                return new ArrayList<Course>();
+            }
+
+            int index = 0;
+            String courseTerm;
+            String courseTitle;
+            String courseTime;
+            String courseDay;
+            String courseLocation;
+            String courseInstructor;
+            String courseCRN;
+            String courseCredit;
+            String courseMajor;
+            String courseArea;
+            String courseSection;
+            String courseClass;
+            String courseUniversity;
+            String courseAttribute;
+            while(index < jsonResponse.length()) {
+                JSONObject object = jsonResponse.getJSONObject(index);
+                courseTerm = object.getString("courseTerm");
+                courseTitle = object.getString("courseTitle");
+                courseTime = object.getString("courseTime");
+                courseDay = object.getString("courseDay");
+                courseLocation = object.getString("courseLocation");
+                courseInstructor = object.getString("courseInstructor");
+                courseCRN = object.getString("courseCRN");
+                courseCredit = object.getString("courseCredit");
+                courseMajor = object.getString("courseMajor");
+                courseArea = object.getString("courseArea");
+                courseSection = object.getString("courseSection");
+                courseClass = object.getString("courseClass");
+                courseUniversity = object.getString("courseUniversity");
+                courseAttribute = object.getString("courseAttribute");
+                list.add(new Course(courseTerm, courseDay, courseMajor, courseTitle, courseCRN, courseArea, courseSection, courseClass, courseTime, courseLocation, courseInstructor, courseUniversity, courseCredit, courseAttribute));
+                ++index;
+            }
+
+        }
+        catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return list;
+    }
+}
